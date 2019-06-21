f60baed73bc9de24d2ae040f89fd1d971aed71cb
isaiyepez <isaiyepez@gmail.com>
2019-06-20 23:26:04 -0500

    Add project files.

---
+++ WCF_WS_Employee/Employee.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Runtime.Serialization
+
+<DataContract>
+Public Class Employee
+    Private m_IdEmployee As Integer
+    Private m_Name As String
+    Private m_LastName As String
+
+    <DataMember>
+    Public Property IdEmployee() As Integer
+        Get
+            Return m_IdEmployee
+        End Get
+        Set(ByVal value As Integer)
+            m_IdEmployee = value
+        End Set
+    End Property
+
+    <DataMember>
+    Public Property Name() As String
+        Get
+            Return m_Name
+        End Get
+        Set(ByVal value As String)
+            m_Name = value
+        End Set
+    End Property
+
+    <DataMember>
+    Public Property LastName() As String
+        Get
+            Return m_LastName
+        End Get
+        Set(ByVal value As String)
+            m_LastName = value
+        End Set
+    End Property
+
+    Public Sub New(ByVal IdEmployee As Integer, ByVal Name As String, ByVal LastName As String)
+        Me.m_IdEmployee = IdEmployee
+        Me.m_Name = Name
+        Me.m_LastName = LastName
+
+    End Sub
+End Class
---
+++ WCF_WS_Employee/Module1.vb
@@ -0,0 +1,42 @@
+﻿Imports System.Runtime.Serialization
+Imports System.ServiceModel.Description
+Imports System.ServiceModel
+Imports System.ServiceModel.Activation
+Imports System.ServiceModel.Web
+
+<ServiceContract>
+Public Interface IService
+
+    <OperationContract()>
+    <WebGet()>
+    Function SeekEmployee() As List(Of Employee)
+End Interface
+
+Public Class Service
+    Implements IService
+    Public Function SeekEmployee() As List(Of Employee) Implements IService.SeekEmployee
+        Dim EmployeeCollection As New List(Of Employee)()
+
+        EmployeeCollection.Add(New Employee(1, "Matt", "Daimon"))
+        EmployeeCollection.Add(New Employee(2, "Arnold", "Mendez"))
+        EmployeeCollection.Add(New Employee(3, "Silvester", "Stallone"))
+        EmployeeCollection.Add(New Employee(4, "Scarlet", "johanson"))
+        EmployeeCollection.Add(New Employee(5, "Jean Claude", "VanDame"))
+        EmployeeCollection.Add(New Employee(6, "Terminator", "Baby"))
+        EmployeeCollection.Add(New Employee(7, "John", "HILL"))
+        EmployeeCollection.Add(New Employee(8, "Steven", "Hall"))
+        EmployeeCollection.Add(New Employee(9, "Renee", "Ruso"))
+        Return EmployeeCollection
+    End Function
+End Class
+
+Module Module1
+
+    Sub Main()
+        Dim SetupWCF As WebServiceHost = New WebServiceHost(GetType(Service), New Uri("http://localhost:8000/"))
+        SetupWCF.Open()
+        Console.WriteLine("Press <Enter> to stop the WCF Self-Hosted")
+        Console.ReadLine()
+    End Sub
+
+End Module
